22fad93493a9de4bdbf2876f7d573596e5008cbe
Maria Cosmen <mcosme00@gmail.com>
2023-05-28 13:28:03 +0900

    Add inverse_of to CaresDog

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,7 @@
 class Appointment < ApplicationRecord
   has_many :appointments_dogs, dependent: :destroy
   has_many :dogs, through: :appointments_dogs
+
   belongs_to :establishment
 
   accepts_nested_attributes_for :appointments_dogs
--- app/models/cares_dog.rb
+++ app/models/cares_dog.rb
@@ -1,4 +1,7 @@
 class CaresDog < ApplicationRecord
   belongs_to :dog
-  belongs_to :care
+  belongs_to :care, inverse_of: :cares_dogs
+  # with this code, whenever I add a care to a dog,
+  # that dog automatically gets added to that specific care
+  # dog.cares_dog.reload
 end
