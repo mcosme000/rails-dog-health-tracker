dd146ab3fb65c7ffc537bac4b8d766ade5f5ef52
Maria Cosmen <mcosme00@gmail.com>
2023-05-29 21:26:50 +0900

    Create dogs_controller and CRUD actions with views

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -7,5 +7,37 @@ class DogsController < ApplicationController
   def show
     @dog = Dog.find(params[:id])
   end
-  
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.users << current_user
+    @dog.save
+    redirect_to dashboard_path
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dog_params)
+    redirect_to dog_path(@dog)
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age, :weight, :birthday)
+  end
 end
