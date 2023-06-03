bbd9a9c895b2887dfa70865989216be95fdbc6cf
Maria Cosmen <mcosme00@gmail.com>
2023-06-03 21:34:40 +0900

    Add relation in Appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,8 +1,5 @@
 class Appointment < ApplicationRecord
-  has_many :appointments_dogs, dependent: :destroy
-  has_many :dogs, through: :appointments_dogs
-
   belongs_to :establishment
 
-  accepts_nested_attributes_for :appointments_dogs
+
 end
